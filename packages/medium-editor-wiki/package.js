adcdeefc88c9e73e0ac0f393ef2215885d00e1c1
Steve X Chen <github@stevexchen.com>
2014-05-30 16:10:28 -0400

    moved medium editor to local

---
+++ packages/medium-editor-wiki/package.js
@@ -0,0 +1,14 @@
+Package.describe({
+	name: "donflopez:medium-editor",
+	summary: "Add medium-editor and medium insert clone to Meteor client side",
+	version: "1.2.0"
+});
+
+Package.on_use(function (api) {
+	api.add_files(['medium-editor.css','medium-editor.js','medium-editor-insert-plugin.css'], 'client');
+
+	if(api.export)
+		api.export('MediumEditor', 'client');
+});
+
+//api.add_files(['medium-editor.css','medium-editor.js', 'medium-editor-insert-plugin.all.min.js','medium-editor-insert-plugin.css'], 'client');
